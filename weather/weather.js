d25b204523faf5788761c550dfed8e5bb78bc4aa
skmuddamsetty <muddamsetty.santhosh@gmail.com>
2018-01-20 17:47:02 -0500

    third commit
    third commit

--- weather/weather.js
+++ weather/weather.js
@@ -1,7 +1,7 @@
 const request = require('request');
 var getWeather = (lat,long,callback) =>{
   request({
-  url: `https://api.darksky.net/forecast/replcaewithyourkeyfromforecast.io/${lat},${long}`,
+  url: `https://api.darksky.net/forecast/f66d472aca9a8039df622b734790003d/${lat},${long}`,
   json: true
 },(error,response,body)=>{
   if(!error && response.statusCode===200){
